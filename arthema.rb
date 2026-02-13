14e3754b8c812b801f3a53b2e9a13a1c1463e02b
Danny Viasus <danjavia@gmail.com>
2026-02-13 12:06:14 -0500

    feat: upgrade to v0.1.5

--- arthema.rb
+++ arthema.rb
@@ -1,8 +1,8 @@
 class Arthema < Formula
   desc "A fast, lightweight, and futuristic Cyberpunk-themed API IDE"
   homepage "https://github.com/Danjavia/arthema"
-  url "https://github.com/Danjavia/arthema/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "10bd1eb43f9b04ee2eb161db0ab16bee25d12ef936aa7177ae18ff68be2d3b9d"
+  url "https://github.com/Danjavia/arthema/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "b6ca70afcea8b49aaa753ce04668f46107dd207c6b3a531fd80188a66cfe51bc"
   license "MIT"
 
   depends_on "rust" => :build
